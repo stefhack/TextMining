74508f13fc6148a7a146147141d1a85492774a23
unknown <duprestephane@hotmail.fr>
2016-01-04 17:14:12 +0100

    test arff with java and decision table classifier

--- src/textmining/TextMining.java
+++ src/textmining/TextMining.java
@@ -5,6 +5,19 @@
  */
 package textmining;
 
+import weka.core.Instances;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.List;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.rules.DecisionTable;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+
 /**
  *
  * @author Stef
@@ -14,8 +27,28 @@ public class TextMining {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
+        BufferedReader reader = new BufferedReader(new FileReader("C:/wamp/www/AllocineHelper/arff/20160104.arff"));
+        Instances data = new Instances(reader);
+        reader.close();
+
+        StringToWordVector wordVector = new StringToWordVector();
+        wordVector.setInputFormat(data);
+        Instances dataFiltered = Filter.useFilter(data, wordVector);
+        dataFiltered.setClassIndex(1);
+
+        Classifier decisionTable = (Classifier) new DecisionTable();
+        String[] options = weka.core.Utils.splitOptions("-X 1 -S \"weka.attributeSelection.BestFirst -D 1 -N 5\"");
+        decisionTable.setOptions(options);
+        decisionTable.buildClassifier(dataFiltered);
+
+        Evaluation eval = new Evaluation(dataFiltered);
+        eval.evaluateModel(decisionTable, dataFiltered);
+        String resume = eval.toSummaryString();
+        String matrique = eval.toMatrixString(resume);
+        System.out.println(resume);
+        System.out.println(matrique);
+
     }
-    
+
 }
